d248f075c0b5a9f43a3fc7773136e6c15b13c4ca
dllo <syvisor94xfj0528@163.com>
2016-05-12 16:17:54 +0800

    Initial Commit

---
+++ MyGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyGit
+//
+//  Created by dllo on 16/5/12.
+//  Copyright © 2016年 邢福杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyGit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyGit
+//
+//  Created by dllo on 16/5/12.
+//  Copyright © 2016年 邢福杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
